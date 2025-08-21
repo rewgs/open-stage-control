ca8b6de74ef6008faab1882b28bef5c17b3a75ae
Alex Ruger <alexrugermusic@gmail.com>
2025-08-21 12:46:19 -0700

    Finished client/events.

---
+++ src/client/events/click.mjs
@@ -0,0 +1,99 @@
+// const iOS = require("../ui/ios");
+// const editor = require("../editor");
+
+import iOS from "../ui/ios";
+import editor from "../editor/index.mjs";
+
+const macOs = (navigator.platform || "").match("Mac");
+var longTouchTimer = false;
+
+let clearLongTouchTimer = function () {
+    if (longTouchTimer) {
+        clearTimeout(longTouchTimer);
+        longTouchTimer = false;
+    }
+};
+
+if (macOs) {
+    // on macOs, ctrl+click generates a contextmenu event
+    // that should be interpreted as a rightclick
+    document.body.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
+        DOM.dispatchEvent(event.target, "fast-right-click", event);
+    });
+} else {
+    document.body.addEventListener("contextmenu", (event) => {
+        if (
+            event.target &&
+            !event.target.classList.contains("ace_content") &&
+            !event.target.tagName.match(
+                /INPUT|TEXTAREA|OSC-CONSOLE|OSC-CONSOLE-MESSAGE/
+            )
+        )
+            event.preventDefault();
+    });
+}
+
+function mouseToFastClick(event) {
+    if (event.pointerType === "touch") return;
+    if (event.button == 2) event.preventDefault();
+
+    var e = event;
+
+    var name = event.button == 2 ? "fast-right-click" : "fast-click";
+
+    DOM.dispatchEvent(e.target, name, e);
+
+    if (
+        !FOCUSABLE &&
+        e.target.tagName !== "INPUT" &&
+        e.target.tagName !== "TEXTAREA" &&
+        e.target.tagName !== "SELECT"
+    ) {
+        console.debug("ELECTRON.BLUR()");
+    }
+}
+
+function touchToFastClick(event) {
+    var e = event.changedTouches[0];
+
+    e.shiftKey = event.shiftKey;
+    e.ctrlKey = event.ctrlKey;
+
+    DOM.dispatchEvent(e.target, "fast-click", e);
+
+    if (e.preventOriginalEvent) event.preventDefault();
+
+    longTouchTimer = setTimeout(() => {
+        var off = DOM.offset(e.target);
+
+        e.offsetX = e.pageX - off.left;
+        e.offsetY = e.pageY - off.top;
+
+        DOM.dispatchEvent(e.target, "fast-right-click", e);
+
+        clearLongTouchTimer();
+    }, 600);
+
+    if (
+        ENV.nofocus &&
+        !editor.enabled &&
+        e.target.tagName !== "INPUT" &&
+        e.target.tagName !== "TEXTAREA" &&
+        e.target.tagName !== "SELECT"
+    ) {
+        console.debug("ELECTRON.BLUR()");
+    }
+}
+
+if (!iOS) document.addEventListener("pointerdown", mouseToFastClick, true);
+document.addEventListener("touchstart", touchToFastClick, {
+    passive: false,
+    capture: true
+});
+
+DOM.addEventListener(
+    document,
+    "touchend touchmove touchcancel",
+    clearLongTouchTimer
+);
---
+++ src/client/events/event-emitter.mjs
@@ -0,0 +1,239 @@
+// var customEvents = {};
+// setTimeout(() => {
+// customEvents["draginit"] =
+//     customEvents["drag"] =
+//     customEvents["dragend"] =
+//         require("./drag");
+// customEvents["resize"] = require("./resize");
+// customEvents["wheel"] = require("./dom-event")("wheel");
+// customEvents["scroll"] = require("./dom-event")("scroll", {
+//     capture: true
+// });
+// customEvents["click"] = require("./dom-event")("click");
+// customEvents["fast-click"] = require("./dom-event")("fast-click");
+// customEvents["focus"] = require("./dom-event")("focus", { capture: true });
+// customEvents["blur"] = require("./dom-event")("blur", { capture: true });
+// customEvents["change"] = require("./dom-event")("change");
+// });
+
+import * as drag from "./drag.mjs";
+import resize from "./resize.mjs";
+
+import { triggerEvent } from "./dom-event.mjs";
+
+let customEvents = {
+    draginit: drag,
+    drag: drag,
+    dragend: drag,
+    resize: resize,
+    wheel: triggerEvent("wheel"),
+    scrool: triggerEvent("scroll", { capture: true }),
+    click: triggerEvent("click"),
+    "fast-click": triggerEvent("fast-click"),
+    focus: triggerEvent("focus", { capture: true }),
+    blur: triggerEvent("blur", { capture: true }),
+    change: triggerEvent("change")
+};
+
+// micro optimisation from eventemitter3
+var has = Object.prototype.hasOwnProperty;
+
+// module.exports = class EventEmitter {
+//     constructor() {
+//         this._customBindings = {};
+//         this._listeners = {};
+//
+//         for (var evt in customEvents) {
+//             this._customBindings[evt] = 0;
+//         }
+//     }
+//
+//     trigger(evt, data) {
+//         if (has.call(this._listeners, evt)) {
+//             // shallow copy in case a listener gets added/removed while looping
+//             var listeners = this._listeners[evt].slice(0);
+//             for (var i = 0, len = listeners.length; i !== len; i++) {
+//                 listeners[i](data);
+//             }
+//         }
+//
+//         // Event bubbling
+//         // DOM events won't bubble
+//         // Virtual events will unless data.stopPropagation is set
+//         if (!data || !data.stopPropagation) {
+//             if (this.parent) this.parent.trigger(evt, data);
+//         }
+//
+//         return this;
+//     }
+//
+//     on(evt, listener, options) {
+//         if (options) {
+//             // Custom event setup
+//             if (has.call(customEvents, evt)) {
+//                 this._customBindings[evt];
+//                 if (this._customBindings[evt] === 0) {
+//                     listener._options = options;
+//                     customEvents[evt].setup.call(this, options);
+//                 }
+//                 this._customBindings[evt]++;
+//             }
+//
+//             // Context storing
+//             if (options.context) listener._context = options.context;
+//         }
+//
+//         // Add listener
+//         if (!has.call(this._listeners, evt)) this._listeners[evt] = [];
+//
+//         var listeners = this._listeners[evt];
+//
+//         if (listeners.indexOf(listener) === -1) listeners.push(listener);
+//
+//         return this;
+//     }
+//
+//     off(evt, listener, context) {
+//         // Remove listener
+//         if (evt && has.call(this._listeners, evt)) {
+//             var listeners = this._listeners[evt];
+//
+//             if (listener) {
+//                 var index = listeners.indexOf(listener);
+//
+//                 if (index !== -1) {
+//                     if (context && context !== listener._context) return this;
+//
+//                     listeners.splice(index, 1);
+//
+//                     // Custom event teardown
+//                     if (has.call(customEvents, evt)) {
+//                         if (this._customBindings[evt] !== 0) {
+//                             this._customBindings[evt]--;
+//                             if (this._customBindings[evt] === 0) {
+//                                 customEvents[evt].teardown.call(
+//                                     this,
+//                                     listener._options
+//                                 );
+//                                 delete listener._options;
+//                             }
+//                         }
+//                     }
+//                 }
+//             } else {
+//                 for (var i = listeners.length - 1; i !== -1; i--) {
+//                     if (!context || context == listeners[i]._context) {
+//                         this.off(evt, listeners[i], context);
+//                     }
+//                 }
+//             }
+//         } else if (!evt) {
+//             for (var k in this._listeners) {
+//                 this.off(k, undefined, context);
+//             }
+//         }
+//
+//         return this;
+//     }
+// };
+
+class EventEmitter {
+    constructor() {
+        this._customBindings = {};
+        this._listeners = {};
+
+        for (var evt in customEvents) {
+            this._customBindings[evt] = 0;
+        }
+    }
+
+    trigger(evt, data) {
+        if (has.call(this._listeners, evt)) {
+            // shallow copy in case a listener gets added/removed while looping
+            var listeners = this._listeners[evt].slice(0);
+            for (var i = 0, len = listeners.length; i !== len; i++) {
+                listeners[i](data);
+            }
+        }
+
+        // Event bubbling
+        // DOM events won't bubble
+        // Virtual events will unless data.stopPropagation is set
+        if (!data || !data.stopPropagation) {
+            if (this.parent) this.parent.trigger(evt, data);
+        }
+
+        return this;
+    }
+
+    on(evt, listener, options) {
+        if (options) {
+            // Custom event setup
+            if (has.call(customEvents, evt)) {
+                this._customBindings[evt];
+                if (this._customBindings[evt] === 0) {
+                    listener._options = options;
+                    customEvents[evt].setup.call(this, options);
+                }
+                this._customBindings[evt]++;
+            }
+
+            // Context storing
+            if (options.context) listener._context = options.context;
+        }
+
+        // Add listener
+        if (!has.call(this._listeners, evt)) this._listeners[evt] = [];
+
+        var listeners = this._listeners[evt];
+
+        if (listeners.indexOf(listener) === -1) listeners.push(listener);
+
+        return this;
+    }
+
+    off(evt, listener, context) {
+        // Remove listener
+        if (evt && has.call(this._listeners, evt)) {
+            var listeners = this._listeners[evt];
+
+            if (listener) {
+                var index = listeners.indexOf(listener);
+
+                if (index !== -1) {
+                    if (context && context !== listener._context) return this;
+
+                    listeners.splice(index, 1);
+
+                    // Custom event teardown
+                    if (has.call(customEvents, evt)) {
+                        if (this._customBindings[evt] !== 0) {
+                            this._customBindings[evt]--;
+                            if (this._customBindings[evt] === 0) {
+                                customEvents[evt].teardown.call(
+                                    this,
+                                    listener._options
+                                );
+                                delete listener._options;
+                            }
+                        }
+                    }
+                }
+            } else {
+                for (var i = listeners.length - 1; i !== -1; i--) {
+                    if (!context || context == listeners[i]._context) {
+                        this.off(evt, listeners[i], context);
+                    }
+                }
+            }
+        } else if (!evt) {
+            for (var k in this._listeners) {
+                this.off(k, undefined, context);
+            }
+        }
+
+        return this;
+    }
+}
+
+export default EventEmitter;
